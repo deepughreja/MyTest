f5e62bea968ecd5cce2e0ae534a3baa6b4108bbc
deepughreja <deepughreja@gmail.com>
2020-06-17 07:54:41 +1100

    Sorting table and Sparker implemetation changes

--- dev-challange-my-solution/index.js
+++ dev-challange-my-solution/index.js
@@ -10,18 +10,16 @@ require('./site/style.css')
 
 // Variables and constants
 var sparklineArray = [1, 2, 3, 4, 5];
-var midPriceList = {};
-const nameIndex = 0;
-const bestBidIndex = 1;
-const bestAskIndex = 2;
-const openBidIndex = 3;
-const openAskIndex = 4;
-const lastChangeAskIndex = 5;
-const lastChangeBidIndex = 6;
-const tableId = 'table';
+var currencyUpdateData = [];
+const sortColumnName = 'lastChangeBid';
+const sortColumnorder = 'asc';
+var intervalTime = 0;
+var intervalLimit = 30;
 const connectionSuccessfullMessage = "Successfully connected to a stomp server.Click on 'Start Live Update' to receive the update from stomp server";
 // Change this to get detailed logging from the stomp library
 global.DEBUG = false;
+
+// URLs
 const url = "ws://localhost:8011/stomp"
 const liveCurrencyUpdateUrl = "/fx/prices";
 const client = Stomp.client(url);
@@ -38,123 +36,128 @@ document.getElementById("btnStartLiveUpdate").addEventListener("click", () => {
   startLiveCurrencyUpdate();
 });
 
+//Function that runs in every second
+function setMidPriceInterval() {
+  setInterval(() => {
+    intervalTime = intervalTime + 1;
+    if (intervalTime > intervalLimit) {
+      intervalTime = 0;
+      clearMidPriceValues();
+    }
+  }, 1000);
+}
+
 // Calls on Start Live Update button click
 function startLiveCurrencyUpdate() {
+  // Timer function that runs in every second
   setMidPriceInterval();
   // subscribing stomp to receive the updated value of price
-  return client.subscribe(liveCurrencyUpdateUrl, (message) => {
-    // Storing Mid Price value for specific interval 
-    storeMidPrice(JSON.parse(message.body));
-    // Fuction responsible for adding new row,Updating column values and updating midprice value
-    processTableData(message.body, false);
+  return client.subscribe(liveCurrencyUpdateUrl, message => stompMessageHandler(message));
+}
+
+function stompMessageHandler(message) {
+  //Parse message body
+  var object = JSON.parse(message.body);
+  //Add or update the data in global array
+  updateLiveData(object);
+  //Apply sorting on array
+  currencyUpdateData.sort(compareValues(sortColumnName, sortColumnorder));
+  // Clears default text
+  clearEmptyRecordText();
+  //Render table after sorting
+  renderTableWithSparkLine(currencyUpdateData, object);
+}
+
+function updateLiveData(object) {
+  const filteredData = currencyUpdateData.filter(obj => obj.name == object.name);
+  if (filteredData.length == 0) {
+    //Adding rows first time in array
+    currencyUpdateData.push({
+      "name": object.name,
+      "bestBid": object.bestBid,
+      "bestAsk": object.bestAsk,
+      "openBid": object.openBid,
+      "openAsk": object.openAsk,
+      "lastChangeAsk": object.lastChangeAsk,
+      "lastChangeBid": object.lastChangeBid,
+      "midPrice": (object.bestBid + object.bestAsk) / 2
+    });
+  }
+  else {
+    // Update rows based on recieved data from stomp
+    var data = filteredData[0];
+    data.bestBid = object.bestBid;
+    data.bestAsk = object.bestAsk;
+    data.openBid = object.openBid;
+    data.openAsk = object.openAsk;
+    data.lastChangeAsk = object.lastChangeAsk;
+    data.lastChangeBid = object.lastChangeBid;
+    data.midPrice = data.midPrice == 0 ? calculateMidPrice(object.bestBid, object.bestAsk) : ((data.midPrice + (calculateMidPrice(object.bestBid, object.bestAsk))) / 2);
+  }
+}
+
+// Clears midprice value to 0 after 30 seconds
+function clearMidPriceValues() {
+  currencyUpdateData.forEach((element, index) => {
+    element.midPrice = 0;
   });
 }
 
-// handler to draw the spark lines
+// Sorting Logic
+function compareValues(key, order = 'asc') {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      return 0;
+    }
+    const varA = (typeof a[key] === 'string')
+      ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string')
+      ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
+
+// calculate Mid Price
+var calculateMidPrice = (bestBid, bestAsk) => (bestBid + bestAsk) / 2;
+//Draw SparkLine
 var drawSparkLine = (sparkElement) => new Sparkline(sparkElement).draw(arrayRotate(sparklineArray));
-// get current datetime
-var getCurrentDate = (today) => today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-//Function runs in every 30 second to set the midprice
-function setMidPriceInterval() {
-  setInterval(() => {
-    processTableData(midPriceList, true);
-    midPriceList = [];
-    var dateStr = getCurrentDate(new Date());
-    document.getElementById('midPriceUpdateTime').innerHTML = dateStr;
-  }, 30000);
-}
-
-// Storing Mid Price value for specific interval 
-function storeMidPrice(message) {
-  var midPrice = (message.bestBid + message.bestAsk) / 2;
-  if (midPriceList[message.name]) {
-    midPriceList[message.name] = (midPriceList[message.name] + midPrice) / 2;
-  }
-  midPriceList[message.name] = midPrice;
-}
-
-// Fuction responsible for adding new row,Updating column values and updating midprice value
-function processTableData(input, isMidPriceUpdate) {
-  // Initialization
-  var message;
-  var isRowFound = false;
-  var table = document.getElementById(tableId);
-  var rows = document.getElementById(tableId).rows//.namedItem("myRow").innerHTML
-
-  // Clears 'No record found' text
-  ClearEmptyRecordText();
-
-  //Iterate over the rows 
-  for (i = 0; i < rows.length; i++) {
-    //Updating midPrice Values
-    if (isMidPriceUpdate) {
-      if (input[table.rows[i].cells[0].innerHTML]) {
-        table.rows[i].cells[7].innerHTML = input[table.rows[i].cells[0].innerHTML] + '<span id="sparkLine"></span>';
-        drawSparkLine(table.rows[i].cells[7].getElementsByTagName('span')[0]);
-      }
-    }
-    else {
-      // Updating the value based on received response from stomp
-      message = JSON.parse(input);
-      if (table.rows[i].cells[nameIndex].innerHTML == message.name) {
-        table.rows[i].cells[bestBidIndex].innerHTML = message.bestBid;
-        table.rows[i].cells[bestAskIndex].innerHTML = message.bestAsk;
-        table.rows[i].cells[openBidIndex].innerHTML = message.openBid;
-        table.rows[i].cells[openAskIndex].innerHTML = message.openAsk;
-        table.rows[i].cells[lastChangeAskIndex].innerHTML = message.lastChangeAsk;
-        table.rows[i].cells[lastChangeBidIndex].innerHTML = message.lastChangeBid;
-        isRowFound = true;
-        break;
-      }
-    }
-  }
-  if (!isMidPriceUpdate) {
-    if (!isRowFound) {
-      // Adding new values first time in the table
-      addCurrencyRow(message);
-    }
-    else {
-      //sorting logic
-      sortTable(tableId, lastChangeBidIndex);
-    }
-  }
-}
-
-//Sorting Logic
-function sortTable(table_id, sortColumn) {
-  var tableData = document.getElementById(table_id).getElementsByTagName('tbody').item(0);
-  var rowData = tableData.getElementsByTagName('tr');
-  for (var i = 0; i < rowData.length - 1; i++) {
-    for (var j = 0; j < rowData.length - (i + 1); j++) {
-      if (Number(rowData.item(j).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, "")) < Number(rowData.item(j + 1).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, ""))) {
-        tableData.insertBefore(rowData.item(j + 1), rowData.item(j));
-      }
-    }
-  }
-}
 
 // Clears 'No record found' text
-function ClearEmptyRecordText() {
+function clearEmptyRecordText() {
   var emptyRow = document.getElementById('emptyRecordBlock');
   if (emptyRow) {
     emptyRow.remove();
   }
 }
 
-// Adding values first time
-function addCurrencyRow(object) {
-  var table = document.getElementById('tBody');
-  var tr = document.createElement('tr');
-  tr.innerHTML = '<td>' + object.name + '</td>' +
-    '<td>' + object.bestBid + '</td>' +
-    '<td>' + object.bestAsk + '</td>' +
-    '<td>' + object.openBid + '</td>' +
-    '<td>' + object.openAsk + '</td>' +
-    '<td>' + object.lastChangeAsk + '</td>' +
-    '<td>' + object.lastChangeBid + '</td>' +
-    '<td>NA</td>';
-  table.appendChild(tr);
+// Render Table Values
+function renderTableWithSparkLine(currecyArray, messageObject) {
+  // Render table based on sorted array
+  var tableBody = document.getElementById('tBody');
+  tableBody.innerHTML = '';
+  currecyArray.forEach(object => {
+    var tr = document.createElement('tr');
+    tr.innerHTML = '<td>' + object.name + '</td>' +
+      '<td>' + object.bestBid + '</td>' +
+      '<td>' + object.bestAsk + '</td>' +
+      '<td>' + object.openBid + '</td>' +
+      '<td>' + object.openAsk + '</td>' +
+      '<td>' + object.lastChangeAsk + '</td>' +
+      '<td>' + object.lastChangeBid + '</td>' +
+      '<td>' + (object.midPrice == 0 ? 'NA' : object.midPrice) + '<span id="sparkLine"></span></td>';
+    tableBody.appendChild(tr);
+    // //Render sparkline
+     drawSparkLine(tr.getElementsByTagName('span')[0]);
+  });
 }
 
 // callback and error handler for stomp
